c5054b389801c99fc9ecdb3956b3ee132c472d5f
yaime07-code <jaime.enrique.galvis@gmail.com>
2025-10-17 01:00:00 +0000

    ex

--- practica2/excel.cpp
+++ practica2/excel.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-        
-        
+        int result = 0;
+        for (char c : columnTitle) {
+            int value = c - 'A' + 1; 
+            result = result * 26 + value; 
+        }
+        return result;
     }
 };
 
 int main() {
     Solution sol;
-    cout << "The column number for " << columnTitle << " is: " << result << endl;
+    string columnTitle;
+
+
+    cout << "Ingrese el titulo de columna (por ejemplo A, AB, ZY): ";
+    cin >> columnTitle;
+
+    int numero = sol.titleToNumber(columnTitle);
+    cout << "El numero correspondiente es: " << numero << endl;
     return 0;
 }
