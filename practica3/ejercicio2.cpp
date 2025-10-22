c0137b1b837d27baf69b817072a4feb9a1b0eacd
maira-05 <mayisp14@gmail.com>
2025-10-21 20:32:57 -0500

    punto2

--- practica3/ejercicio2.cpp
+++ practica3/ejercicio2.cpp
@@ -1,4 +1,36 @@
 //Ejercicio 2: Sistema de Alquiler de Vehículos
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+
+class Vehiculo {
+    private:
+    string marca;
+    string modelo;
+    string placa;
+    bool disponible;
+
+    public:
+    Vehiculo (string m, string mo, string p){
+        marca = m;
+        modelo = mo;
+        placa = p;
+        disponible=true;
+    }
+    virtual ~Vehiculo(){}
+    virtual void mostrarInformacion() const {
+        cout << "Marca: "<<marca<<", Modelo: "<<modelo<< ",Placa: "<<placa<<", Disponible: "<< (disponible ? "Si":"No")<<endl;
+    }
+    
+
+};
+class Auto {
+
+};
+class Bicicleta {
+
+};
+class SistemaAlquiler {
+
+};
