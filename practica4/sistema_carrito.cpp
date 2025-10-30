35e79fcda614a3f7fea0eef7dd964c40836817ee
maira-05 <mayisp14@gmail.com>
2025-10-29 20:42:36 -0500

    carrito

---
+++ practica4/sistema_carrito.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Producto {
+    private:
+};
+class ItemCarrito {
+    private:
+}; 
+class CarritoCompras {
+    private:
+};
+class Usuario {
+    private:
+};
+int main(){
+
+}
