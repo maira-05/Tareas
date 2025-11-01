1ff32e774ef12e5ad0d3b4c8bd5cdae2684b1203
yaime07 <jaime.enrique.galvis@gmail.com>
2025-11-01 11:15:06 -0500

    parcial_f2

---
+++ parcial/build/CMakeFiles/gestion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gestion.dir/src/Estudiante.cpp.o"
+  "CMakeFiles/gestion.dir/src/Estudiante.cpp.o.d"
+  "CMakeFiles/gestion.dir/src/main.cpp.o"
+  "CMakeFiles/gestion.dir/src/main.cpp.o.d"
+  "gestion"
+  "gestion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gestion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ parcial/build/CMakeFiles/gestion.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for gestion.
+# This may be replaced when dependencies are built.
