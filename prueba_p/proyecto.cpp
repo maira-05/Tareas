987fc03b94203496eaab64553e2efebe4d34f14e
maira-05 <mayisp14@gmail.com>
2025-11-16 20:30:43 -0500

    menu

--- prueba_p/proyecto.cpp
+++ prueba_p/proyecto.cpp
@@ -20,5 +20,37 @@ class CSVExporter {
 };
 int main() {
     cout << "Proyecto de gestion de registros de salud." << endl;
+    while (true) {
+        cout << "\n===== MENU PRINCIPAL =====\n";
+        cout << "1. Registrarse\n";
+        cout << "0. Salir del programa\n";
+        cout << "Seleccione una opción: ";
+         int opcion;
+        cin >> opcion;
+
+        if (opcion == 0) {
+            cout << "Saliendo del programa...\n";
+            break;
+        }
+
+        if (opcion != 1) {
+            cout << "Opción inválida. Intente de nuevo.\n";
+            continue;
+        }
+        
+        string usuario;
+        cin.ignore();
+        cout << "Ingrese su usuario: ";
+        getline(cin, usuario);
+        cout << "Ingrese su ID: ";
+        int id;
+        cin >> id;
+        cin.ignore();
+        cout << "Ingrese su contraseña: ";
+        string contrasena;
+        getline(cin, contrasena);
+
+        User* usuarioActual = new User();
+    }
     return 0;
 }
