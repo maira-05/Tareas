9334ad440039bda3226098329253ccc203ad8480
maira-05 <mayisp14@gmail.com>
2025-11-16 19:40:25 -0500

    clases

---
+++ prueba_p/proyecto.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class User {
+
+};
+class HealthRecord {
+
+};
+class DatabaseManager {
+
+};
+class HealthAnalyzer {
+
+};
+class CSVExporter {
+
+};
+int main() {
+    cout << "Proyecto de gestion de registros de salud." << endl;
+    return 0;
+}
