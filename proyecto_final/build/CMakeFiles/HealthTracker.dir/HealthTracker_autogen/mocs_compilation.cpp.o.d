916fbe206186ea7d0c5e4566076a56e966f37b1f
yaime07 <jaime.enrique.galvis@gmail.com>
2025-12-02 14:50:03 -0500

    cmake2

--- proyecto_final/build/CMakeFiles/HealthTracker.dir/HealthTracker_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HealthTracker.dir/HealthTracker_autogen/mocs_compilation.cpp.o: \
- /home/maira/Tareas/proyecto_final/build/HealthTracker_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- proyecto_final/src/main.cpp
+++ proyecto_final/src/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QIcon>
 #include "DatabaseManager.h"
 #include "LoginDialog.h"
 #include "MainWindow.h"
@@ -6,6 +7,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Ícono de la aplicación
+    a.setWindowIcon(QIcon(":/icon.png"));
 
     DatabaseManager db("health.db");
 
